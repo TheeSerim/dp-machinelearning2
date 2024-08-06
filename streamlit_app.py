0969a8ec4135b44eb1a13034b581db6799714689
TheeSerim <ndahulilwas@gmail.com>
2024-08-06 13:30:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pickle
 
-#model = pickle.load(open('./content/model_loan2.pkl', 'rb'))
+#model = pickle.load(open('./master/model_loan2.pkl', 'rb'))
+#https://github.com/TheeSerim/dp-machinelearning2/blob/master/model_loan2.pkl
 
 st.title('🎈 Loan Application App')
 
@@ -11,12 +12,17 @@ st.info('This App is build to determine if a client will be approved or rejected
 user_input = st.text_input("Enter your name")
 st.write(f"Hello, {user_input}!")
 
-# Number input
-number = st.number_input("Enter a Loan_amount", min_value=0, max_value=10000000, value=0)
-st.write(f"The number you entered is {number}")
+# Number input for Loan_amount
+Loan Amount = st.number_input("Enter a Loan_amount", min_value=0, max_value=10000000, value=0)
+st.write(f"The number you entered is {Loan Amount}")
 
 # Select box for education
 options = ["Education = Yes", "Education = No"]
 selected_option = st.selectbox("Choose an option", options)
 st.write(f"Selected: {selected_option}")
+
+# Number input for Credit Score (Cibil_score)
+number = st.number_input("Enter your Credit score", min_value=0, max_value=10000000, value=0)
+st.write(f"The number you entered is {number}")
+
 
