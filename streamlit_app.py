7a3fd46ecc7923dc15f0b27037a6199ed8e5fca3
TheeSerim <ndahulilwas@gmail.com>
2024-08-06 02:32:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,16 @@ st.title('🎈 Loan Application App')
 
 st.info('This App is build to determine if an client will be approved or rejected for loan application')
 
+# Text input
+user_input = st.text_input("Enter your name")
+st.write(f"Hello, {user_input}!")
+
+# Number input
+number = st.number_input("Enter a Loan_amount", min_value=0, max_value=10 000 000, value=0)
+st.write(f"The number you entered is {number}")
+
+# Select box for education
+options = ["Education = Yes", "Education = No"]
+selected_option = st.selectbox("Choose an option", options)
+st.write(f"Selected: {selected_option}")
 
