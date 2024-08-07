f53d77bd7c78fc9a8b8c77adcab3e96f697e9726
TheeSerim <ndahulilwas@gmail.com>
2024-08-08 00:12:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,18 +69,18 @@ def run_loan():
 
 
 
-   user_input = [[no_of_dep,edu_options,emp_options,annual_income,Loan_Amount,loan_term,res_assets,com_assets,lux_assets,bank_assets,Credit_score]]
-   print(user_input)
-   #Create a data frame for the user inputs
-   user_input_df = pd.DataFrame(user_input)
+    user_input = [[no_of_dep,edu_options,emp_options,annual_income,Loan_Amount,loan_term,res_assets,com_assets,lux_assets,bank_assets,Credit_score]]
+    print(user_input)
+    #Create a data frame for the user inputs
+    user_input_df = pd.DataFrame(user_input)
 
-   prediction = model.predict(user_input_df.values)
-   lc = [str(i) for i in prediction]
-   ans = int("".join(lc))
+    prediction = model.predict(user_input_df.values)
+    lc = [str(i) for i in prediction]
+    ans = int("".join(lc))
 if ans == 0:
-   st.error("Sorry, you are not eligible for a loan at this moment")
+    st.error("Sorry, you are not eligible for a loan at this moment")
 else:
-   st.success("Congratulations you are eligible for a loan")
+    st.success("Congratulations you are eligible for a loan")
 if st.button("Submit"):
 run_loan()
  
