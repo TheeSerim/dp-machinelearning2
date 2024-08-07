e88b836fcefd444ef981121a6dd0b6ad8e9b2da1
TheeSerim <ndahulilwas@gmail.com>
2024-08-07 16:52:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,12 @@ import numpy as np
 import pickle
 import os
 import pandas as pd
-!pip install lightgbm
-pip install 'lightgbm[pandas]'
+
 
 
 # Load the pickle data from the downloaded content
-pickle_in = open("LGBM_tuned-2", 'rb')
-model = pickle.load(pickle_in)
+#pickle_in = open("LGBM_tuned-2", 'rb')
+#model = pickle.load(pickle_in)
 
 
           
