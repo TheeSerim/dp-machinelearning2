9154f89e3c1bf6da129df31720c7773d76e87c2c
TheeSerim <ndahulilwas@gmail.com>
2024-08-08 02:46:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,12 @@ def run_loan():
     st.write(f"Hello, {client_name}!")
 
     # Text input
-    # Number input for Loan_amount
-    Loan_Amount = st.number_input("Enter a Loan_amount", min_value=0, max_value=10000000, value=0)
-    st.write(f"The loan amount you would like to apply for is {Loan_Amount}")
+    
 
+    #Number input for applicant's number of dependents
+    no_of_dependents = st.number_input("Enter your number of dependents", min_value=0, max_value=5, value=0)
+    st.write(f"The number of dependents entered is {no_of_dependents}")
+    
     #Select box for education
     #edu_options = ["Education = Yes", "Education = No"]
     #selected_option = st.selectbox("Choose an option", edu_options)
@@ -34,77 +36,68 @@ def run_loan():
     st.write(f"Selected: {education}")
 
     #Select box for self-employment
-    emp_options = ["self_employ = Yes", "self_employ = No"]
-    selected_option = st.selectbox("Choose an option", emp_options)
+    self_employed = ["self_employ = Yes", "self_employ = No"]
+    selected_option = st.selectbox("Choose an option", self_employed)
     st.write(f"Selected: {selected_option}")
 
-    #Number input for Credit Score (Cibil_score)
-    Credit_score = st.number_input("Enter your Credit score", min_value=300, max_value=850, value=300)
-    st.write(f"Your credit score entered is {Credit_score}")
-
     #Number input for applicant's annual income
-    annual_income = st.number_input("Enter your annual income", min_value=0, max_value=10000000, value=0)
-    st.write(f"Your annual income entered is {annual_income}")
+    income_annum = st.number_input("Enter your annual income", min_value=0, max_value=10000000, value=0)
+    st.write(f"Your annual income entered is {income_annum}")
 
+    #Number input for Loan_amount
+    loan_amount = st.number_input("Enter a Loan_amount", min_value=0, max_value=10000000, value=0)
+    st.write(f"The loan amount you would like to apply for is {loan_amount}")
 
     #Selectbox for loan term
     loan_term_options = list(range(2, 21, 2))
     loan_term = st.selectbox("Enter your loan term in months", loan_term_options, index=0)
     st.write(f"Your selected loan duration is {loan_term} months")
 
-    #Number input for applicant's number of dependents
-    no_of_dep = st.number_input("Enter your number of dependents", min_value=0, max_value=5, value=0)
-    st.write(f"The number of dependents entered is {no_of_dep}")
-
     #Number input for applicant's Residential asset values
-    res_assets = st.number_input("Enter your residential asset value", min_value=0, max_value=50000000, value=0)
-    st.write(f"The residential asset value entered is {res_assets}")
+    residential_assets_value = st.number_input("Enter your residential asset value", min_value=0, max_value=50000000, value=0)
+    st.write(f"The residential asset value entered is {residential_assets_value}")
 
     #Number input for applicant's commercial asset values
-    com_assets = st.number_input("Enter your commercial asset value", min_value=0, max_value=40000000, value=0)
-    st.write(f"The commercial asset value entered is {com_assets}")
+    commercial_assets_value = st.number_input("Enter your commercial asset value", min_value=0, max_value=40000000, value=0)
+    st.write(f"The commercial asset value entered is {commercial_assets_value}")
 
     #Number input for applicant's luxury asset values
-    lux_assets = st.number_input("Enter your luxury asset value", min_value=0, max_value=50000000, value=0)
-    st.write(f"The luxury asset value entered is {lux_assets}")
+    luxury_assets_value = st.number_input("Enter your luxury asset value", min_value=0, max_value=50000000, value=0)
+    st.write(f"The luxury asset value entered is {luxury_assets_value}")
 
     #Number input for applicant's bank asset values
-    bank_assets = st.number_input("Enter your bank asset value", min_value=0, max_value=50000000, value=0)
-    st.write(f"The bank asset value entered is {bank_assets}")
+    bank_asset_value = st.number_input("Enter your bank asset value", min_value=0, max_value=50000000, value=0)
+    st.write(f"The bank asset value entered is {bank_asset_value}")
+
+    #Number input for Credit Score (Cibil_score)
+    capped_credit_score = st.number_input("Enter your Credit score", min_value=300, max_value=850, value=300)
+    st.write(f"Your credit score entered is {capped_credit_score}")
 
 
 if st.button("Submit"):
-   #Create a DataFrame for user inputs
-   user_data = pd.DataFrame({
-   "no_of_dep": [no_of_dependents],
-   "edu_options":[education],
-   "emp_options": [self_employed],
-   "annual_income" : [income_annum],
-   "Loan_Amount" : [loan_amount],
-   "loan_term" : [loan_term],
-   "Credit_score" : [capped_credit_score],
-   "res_assets" : [residential_assets_value],
-   "com_assets" : [commercial_assets_value],
-   "lux_assets" : [luxury_assets_value],
-   "bank_assets" : [bank_asset_value]
-   })
-   user_input = [[no_of_dep,edu_options,emp_options,annual_income,Loan_Amount,loan_term,res_assets,com_assets,lux_assets,bank_assets,Credit_score]]
-   print(user_input)
-   #Create a data frame for the user inputs
-   user_input_df = pd.DataFrame(user_input)
+   columns = ['no_of_dependents','education','self_employed','income_annum','loan_amount','loan_term','residential_assets_value','commercial_assets_value','luxury_assets_value','bank_asset_value','capped_credit_score']
 
-   prediction = model.predict(user_input_df.values)
-   lc = [str(i) for i in prediction]
-   ans = int("".join(lc))
-   if  ans == 0:
-       st.error("Sorry, you are not eligible for a loan at this moment")
-   else:
-       st.success("Congratulations you are eligible for a loan")
+   
+   #Create a data frame for the user inputs
+   #user_input_df = pd.DataFrame(user_input)
+def predict():
+    col= np.array([no_of_dependents,education,self_employed,income_annum,loan_amount,
+                   loan_term,residential_assets_value,commercial_assets_value,luxury_assets_value,bank_asset_value,capped_credit_score])
+    data= pd.DataFrame([col],columns=columns)
+    prediction = model.predict(data.values)
+    lc = [str(i) for i in prediction]
+    ans = int("".join(lc))
+    if  ans == 0:
+        st.error("Sorry, you are not eligible for a loan at this moment")
+    else:
+        st.success("Congratulations you are eligible for a loan")
     
     
 run_loan()
  
 
+   #user_input = [[no_of_dep,edu_options,emp_options,annual_income,Loan_Amount,loan_term,res_assets,com_assets,lux_assets,bank_assets,Credit_score]]
+ 
 
    #Create a DataFrame for user inputs
    #user_data = pd.DataFrame({
