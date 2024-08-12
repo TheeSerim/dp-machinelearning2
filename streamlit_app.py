1e164b2d0ec48eeb95ac4eb0daad7f95b3d664cf
TheeSerim <ndahulilwas@gmail.com>
2024-08-12 11:59:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ def run_loan():
 
    
 if st.button("Submit"):
-    output = predict_loan()
+    output = predict_loan(no_of_dependents,education_numeric,self_employed_numeric,income_annum,loan_amount,loan_term,residential_assets_value,commercial_assets_value,luxury_assets_value,bank_asset_value,capped_credit_score)
     st.success('The loan application outcome is {}'.formate(output))
     if output == 0:
        st.error("Sorry, you are not eligible for a loan at this moment")
